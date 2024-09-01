bda8df6356bc640d7e9638078c6624b4253b60ed
Roman Shahtarov <abaltis@yandex.ru>
2024-09-01 17:15:17 +0300

    fix/deploy

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Деплой приложения на сервер с использованием pm2
 
-Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+Автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
+
+IP адрес 89.169.141.196
+Frontend https://mesto-dev.projects.nomorepartiesco.ru
+Backend https://api.mesto-dev.projects.nomorepartiesco.ru
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,14 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import { errors } from 'celebrate';
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import { errors } from "celebrate";
+import helmet from "helmet";
 // import cors from 'cors';
-import errorHandler from './middlewares/error-handler';
-import { DB_ADDRESS } from './config';
-import routes from './routes';
+import errorHandler from "./middlewares/error-handler";
+import { DB_ADDRESS } from "./config";
+import routes from "./routes";
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,10 +18,11 @@ mongoose.connect(DB_ADDRESS);
 // app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(cookieParser());
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
 
 // eslint-disable-next-line no-console
-app.listen(PORT, () => console.log('ok'));
+app.listen(PORT, () => console.log("ok"));
